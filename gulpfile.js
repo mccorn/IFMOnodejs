d36aadf941d6d6d8baa6ea3821f2bb645a5129db
mccorn <mccorn13@gmail.com>
2018-04-15 03:33:48 +0300

    Fixed positioning,classes naming and m.o.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'); 
 var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
 
 //Задача умолчанию, перед которой выполнится 
 //задача 'Запуск сервера'('browser-sync'):
@@ -9,16 +10,23 @@ gulp.task('default',['browser-sync'], function () {
 });
 
 //Задача 'build':
-gulp.task('build', function () {
+gulp.task('build',['sass'], function () {
     return gulp.src('site/**/*')
-           .pipe(gulp.dest('../public'));
+           .pipe(gulp.dest('../public/site'));
 });
 
 //Новая задача 'Запуск сервера'('browser-sync'):
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
-            baseDir: "../public"
+            baseDir: "../public/site"
         }
     });
+});
+
+//Препроцессор 'sass':
+gulp.task('sass', function () {
+  return gulp.src('site/sass/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('../public/site/css'));
 });
