e43904f8f01ac1a514121c02599ed20175138a18
mccorn <mccorn13@gmail.com>
2018-05-02 03:28:00 +0300

    Added function for Task #2

--- JSHomework/lesson-2/script__day-2.js
+++ JSHomework/lesson-2/script__day-2.js
@@ -21,8 +21,22 @@ function testTask_1() {
     corn.alert(messagestr);
 };
 
-function testTask_2() {
-    corn.alert('Error: у меня выходной!');
+function testTask_2(x1,y1,z1,x2,y2,z2,x3,y3,z3) {
+//    corn.alert('Error: у меня выходной!');
+//    alert(`x1 = ${x1}
+//x2 = ${x2}
+//x3 = ${x3}
+//y1 = ${y1}
+//y2 = ${y2}
+//y3 = ${y3}
+//z1 = ${z1}
+//z2 = ${z2}
+//z3 = ${z3}`);
+    var par12 = Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2);
+    var par13 = Math.abs(Math.sqrt((x1 - x3) ** 2 + (y1 - y3) ** 2 + (z1 - z3) ** 2));
+    var par23 = Math.sqrt((x3 - x2) ** 2 + (y3 - y2) ** 2 + (z3 - z2) ** 2);
+    corn.alert(par12 + ' - ' + parseInt(par23) + ' - ' + par13);
+    corn.alert(`Result: ${isRightTriangle(par12, par13, par23)}`); 
 };
 
 function testTask_3() {
@@ -122,6 +136,13 @@ function testTask_7() {
     
     corn.alert(messagestr);
 };
+
+function isRightTriangle(par1, par2, par3) {
+    var arr = [par1, par2, par3];
+    arr.sort();
+    return arr[2] ** 2 == arr[1] ** 2 + arr[0] ** 2;
+}
+
 
 function isBissextile(par1) {
     return !((par1 % 4) || (!(par1 % 100) && (par1 % 400)));
